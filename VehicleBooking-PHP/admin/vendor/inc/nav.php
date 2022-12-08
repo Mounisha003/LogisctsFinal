14b7d60ed1eb1bdfab34e5e1915fca1aab71c5af
Mounisha003 <mounishakln@gmail.com>
2022-12-08 16:16:31 +0530

    second commit

---
+++ VehicleBooking-PHP/admin/vendor/inc/nav.php
@@ -0,0 +1,39 @@
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container">
+      <a class="navbar-brand" href="index.php">Priya Transport</a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="about.php">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="services.php">Features</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="contact.php">Contact</a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="gallery.php">Gallery</a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="https://www.google.com/maps/place/PRIYA+TRANSPORTS/@12.7093497,77.7982342,17z/data=!3m1!4b1!4m5!3m4!1s0x3bae70831a8cc7f3:0x7762071dd67d8251!8m2!3d12.7092991!4d77.8004103">Location</a>
+          </li>
+
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownBlog" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+               Login Panel
+            </a>
+            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownBlog">
+              <a class="dropdown-item" href="admin/">Admin Login</a>
+              <a class="dropdown-item" href="usr/">Client Login</a>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
